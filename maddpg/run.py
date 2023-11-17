3c19b351937a4ee3bea9c049bcfeb231f026fbea
HarshImpala <115982138+HarshImpala@users.noreply.github.com>
2023-11-17 20:06:24 +0100

    Uploaded maddpg folder
    enjoy

---
+++ maddpg/run.py
@@ -0,0 +1,139 @@
+import numpy as np
+from maddpg import MADDPG
+from buffer import MultiAgentReplayBuffer
+from pettingzoo.mpe import simple_speaker_listener_v4, simple_tag_v3
+import time
+
+def obs_list_to_state_vector(observation):
+    state = np.array([])
+    for obs in observation:
+        state = np.concatenate([state, obs])
+    return state
+
+
+def run():
+    # parallel_env = simple_speaker_listener_v4.parallel_env(
+            # continuous_actions=True, render_mode="human")
+
+
+    EVAL_INTERVAL = 1000
+    MAX_STEPS = 40000
+    evaluate_performance = False
+    best_score = 0
+    total_steps = 0
+    episode = 0
+    eval_scores = []
+    eval_steps = []
+
+    render_mode = "human" if evaluate_performance == True else "none"
+
+    parallel_env = simple_tag_v3.parallel_env(num_good=1, num_adversaries=3,
+                                              num_obstacles=0, continuous_actions=True, render_mode=render_mode)
+    _, _ = parallel_env.reset()
+    n_agents = parallel_env.max_num_agents
+
+    # print(n_agents)
+
+    actor_dims = []
+    n_actions = []
+    for agent in parallel_env.agents:
+        # print(f'agent is: {agent}, actor dims is: {parallel_env.observation_space(agent).shape[0]}')
+        actor_dims.append(parallel_env.observation_space(agent).shape[0])
+        n_actions.append(parallel_env.action_space(agent).shape[0])
+        # print(f'agent is: {agent}, actions is: {parallel_env.action_space(agent).shape[0]}')
+    critic_dims = sum(actor_dims) + sum(n_actions)
+
+    maddpg_agents = MADDPG(actor_dims, critic_dims, n_agents, n_actions,
+                           env=parallel_env, gamma=0.95, alpha=1e-4, beta=1e-3)
+    critic_dims = sum(actor_dims)
+    memory = MultiAgentReplayBuffer(5_000_000, critic_dims, actor_dims, # changed to 5m
+                                    n_actions, n_agents, batch_size=1024)
+
+
+
+    score = evaluate(maddpg_agents, parallel_env, episode, total_steps)
+    eval_scores.append(score)
+    eval_steps.append(total_steps)
+
+    if evaluate_performance:
+        maddpg_agents.load_checkpoint()
+
+    while total_steps < MAX_STEPS:
+        obs, _ = parallel_env.reset()
+        terminal = [False] * n_agents
+        # score = 0
+        while not any(terminal):
+            if evaluate_performance:
+                time.sleep(0.25)  # to slow down the action
+
+            actions = maddpg_agents.choose_action(obs)
+
+            obs_, reward, done, trunc, info = parallel_env.step(actions)
+
+            list_done = list(done.values())
+            list_obs = list(obs.values())
+            list_reward = list(reward.values())
+            list_actions = list(actions.values())
+            list_obs_ = list(obs_.values())
+            list_trunc = list(trunc.values())
+
+            state = obs_list_to_state_vector(list_obs)
+            state_ = obs_list_to_state_vector(list_obs_)
+
+            terminal = [d or t for d, t in zip(list_done, list_trunc)]
+            memory.store_transition(list_obs, state, list_actions, list_reward,
+                                    list_obs_, state_, terminal)
+
+            if total_steps % 100 == 0 and not evaluate_performance:
+                maddpg_agents.learn(memory)
+            obs = obs_
+            total_steps += 1
+
+        if total_steps % EVAL_INTERVAL == 0:
+            score = evaluate(maddpg_agents, parallel_env, episode, total_steps)
+            eval_scores.append(score)
+            eval_steps.append(total_steps)
+
+        if not evaluate_performance:
+            if score > best_score:
+                print("saving checkpoint")
+                maddpg_agents.save_checkpoint()
+                best_score = score
+
+
+        episode += 1
+
+    np.save('data/maddpg_scores.npy', np.array(eval_scores))
+    np.save('data/maddpg_steps.npy', np.array(eval_steps))
+
+
+def evaluate(agents, env, ep, step, n_eval=3):
+    score_history = []
+    for i in range(n_eval):
+        obs, _ = env.reset()
+        score = 0
+        terminal = [False] * env.max_num_agents
+        while not any(terminal):
+            actions = agents.choose_action(obs, evaluate=True)
+            obs_, reward, done, trunc, info = env.step(actions)
+
+            list_trunc = list(trunc.values())
+            list_reward = list(reward.values())
+            list_done = list(done.values())
+
+            terminal = [d or t for d, t in zip(list_done, list_trunc)]
+
+            obs = obs_
+            score += sum(list_reward)
+        score_history.append(score)
+    avg_score = np.mean(score_history)
+
+
+    print(f'Evaluation episode {ep} train steps {step}'
+          f' average score {avg_score:.1f}')
+    return avg_score
+
+
+if __name__ == '__main__':
+    run()
+    # evaluate()
---
+++ maddpg/run_trained_network.py
@@ -0,0 +1,117 @@
+import numpy as np
+import torch as T
+from maddpg import MADDPG
+from buffer import MultiAgentReplayBuffer
+from pettingzoo.mpe import simple_speaker_listener_v4, simple_tag_v3
+from networks import ActorNetwork
+import time
+
+def obs_list_to_state_vector(observation):
+    state = np.array([])
+    for obs in observation:
+        state = np.concatenate([state, obs])
+    return state
+
+
+EVAL_INTERVAL = 1000
+MAX_STEPS = 40000
+evaluate_performance = True
+best_score = 0
+total_steps = 0
+episode = 0
+eval_scores = []
+eval_steps = []
+
+
+
+env = simple_tag_v3.env(num_good=1, num_adversaries=3, max_cycles=40000, continuous_actions=False,
+                                              num_obstacles=0, render_mode="human")
+env.reset(seed=42)
+n_agents = env.max_num_agents
+
+"""
+    def __init__(self, actor_dims, critic_dims, n_actions,
+                 n_agents, agent_idx, chkpt_dir, min_action,
+                 max_action, alpha=1e-4, beta=1e-3, fc1=64,
+                 fc2=64, gamma=0.95, tau=0.01):
+        self.gamma = gamma
+        self.tau = tau
+        self.n_actions = n_actions
+        agent_name = 'agent_%s' % agent_idx
+        self.agent_idx = agent_idx
+        self.min_action = min_action
+        self.max_action = max_action
+
+        self.actor = ActorNetwork(alpha, actor_dims, fc1, fc2, n_actions,
+                                  chkpt_dir=chkpt_dir,
+                                  name=agent_name+'_actor')
+"""
+"""
+actor_dims = []
+n_actions = []
+for agent in parallel_env.agents:
+    actor_dims.append(parallel_env.observation_space(agent).shape[0])
+    n_actions.append(parallel_env.action_space(agent).shape[0])
+"""
+# load up the 4 networks
+fc1 = 64
+fc2 = 64
+adversary_dims = 12
+agent_dims = 10
+n_actions = 5
+alpha = 1e-4
+device = 'cuda:0' if T.cuda.is_available() else 'cpu'
+
+adversary_0_network = ActorNetwork(alpha, adversary_dims, fc1, fc2, n_actions,
+                                  chkpt_dir="/tmp/maddpg/simple_tag/agent_0_actor",
+                                  name="agent_0"+'_target_actor')
+adversary_1_network = ActorNetwork(alpha, adversary_dims, fc1, fc2, n_actions,
+                                  chkpt_dir="/tmp/maddpg/simple_tag/agent_0_actor",
+                                  name="agent_1"+'_target_actor')
+adversary_2_network = ActorNetwork(alpha, adversary_dims, fc1, fc2, n_actions,
+                                  chkpt_dir="/tmp/maddpg/simple_tag/agent_0_actor",
+                                  name="agent_2"+'_target_actor')
+agent_0_network = ActorNetwork(alpha, agent_dims, fc1, fc2, n_actions,
+                                  chkpt_dir="/tmp/maddpg/simple_tag/agent_0_actor",
+                                  name="agent_3"+'_target_actor')
+# print(next(agent_0_network.parameters()).is_cuda)
+# agent_0_network = agent_0_network.to(device)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+    t_obs = T.tensor(observation)
+    action = None
+    # time.sleep(0.01)  # to slow down the action
+    # print(f'obs is: {t_obs}')
+    t_obs = t_obs.to(device)
+    # print(t_obs.is_cuda)
+
+    if agent == "agent_0":
+
+        possible_actions = agent_0_network(t_obs)
+        possible_actions = possible_actions.to("cpu")
+        action = np.argmax(possible_actions.detach().numpy())
+        # action = 0
+        # print(possible_actions)
+        # print(max_action)
+    elif agent == "adversary_0":
+        possible_actions = adversary_0_network(t_obs)
+        possible_actions = possible_actions.to("cpu")
+        action = np.argmax(possible_actions.detach().numpy())
+        # action = 0
+    elif agent == "adversary_1":
+        possible_actions = adversary_1_network(t_obs)
+        possible_actions = possible_actions.to("cpu")
+        action = np.argmax(possible_actions.detach().numpy())
+        # action = 0
+    elif agent == "adversary_2":
+        possible_actions = adversary_2_network(t_obs)
+        possible_actions = possible_actions.to("cpu")
+        action = np.argmax(possible_actions.detach().numpy())
+        # action = 0
+    if termination or truncation:
+        action = None
+
+    env.step(action)
+env.close()
+
